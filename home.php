36e083205d4b9f13b7cd146ecf83ab78124066f6
Harshi2303 <162798858+Harshi2303@users.noreply.github.com>
2024-10-05 16:34:31 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Aventura</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500" rel="stylesheet">
+	<!--Bootstrap CSS-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="override.css">
+	<!-- First include jquery js -->
+<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+
+<!-- Then include bootstrap js -->
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+  <script type="text/javascript" src="js/changebg.js" async></script>
+</head>
+<body>
+    <div class="content">
+      <header>
+        <h2><a href="#">AVENTURA</a></h2>
+        <nav>
+          <li><a href="register.html">Get Started</a></li>
+          <li><a href="about-us.html">About</a></li>
+
+        </nav>
+      </header>
+
+      <div class="Tagline">
+        <h1>EXPLORE BEYOND BOUNDARIES</h1>
+        <h3>Embark on Endless Adventures and Create Timeless Memories</h3>
+        <br> <br> 
+        <div class="buttons-flex">
+          <a href="userlogin.html" class="button">Login</a>
+          <a href="register.html" class="button">Sign Up</a>
+        </div>
+      </div>
+    </div>
+
+
+		<div class="survey">
+			<div class="surveyText"> Can't decide where to go on your holiday? Take a quick survey and get instant recommendation of places in India to visit!</div>
+			<div class="surveyBtn"><a href="survey.php"><button class="yellowBtn">Take Survey</button></a></div>
+		</div>
+	</div> <!--home ends-->
+
+	
+  
+    <!--footer-->
+    	<footer class="page-footer font-small pt-4">
+    	<div class="container-fluid text-center text-md-left">
+      		<div class="row">
+        		<div class="col-md-6 mt-md-0 mt-3">
+          			<h6 class="footerText">Contact Us</h6>
+          			<p class="footerText"><i class="fa fa-envelope icon" aria-hidden="true"></i> aventura.com</p>
+          			<p class="footerText"><i class="fa fa-phone icon" aria-hidden="true"></i> 1800-676-333 (9:00 am to 9:00 pm)</p>	
+        		</div>
+        		<hr class="clearfix w-100 d-md-none pb-3">
+        		<div class="col-md-3 mb-md-0 mb-3">
+          		</div>
+         	 	<div class="col-md-3 mb-md-0 mb-3">
+            		<ul class="list-unstyled">
+             			<li><a href="#!" class=" footerText">About Us</a></li>
+              			<li><a href="#!" class=" footerText">Terms and Conditions</a></li>
+              			<li><a href="#!" class=" footerText">Privacy Policy</a></li>
+            		</ul>
+          		</div>
+      		</div>
+    	</div>
+    	
+    	
+    	<div class="footer-copyright text-center py-3 madeby">
+    		<font style="color:  #15a18ef4"> AVENTURA &copy; 2024</font>
+    	</div>
+  	</footer>
+
+</body>
+</html>
+
+
